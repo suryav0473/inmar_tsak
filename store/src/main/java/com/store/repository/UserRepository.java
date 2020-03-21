0760cea79e3d62dfcdb2d3ad3598282dd0039b42
SuryaV <SuryaV@DESKTOP-R94UNA8>
2020-03-21 12:57:19 +0530

    Status Added at User Repository for User Login Validations

--- store/src/main/java/com/store/repository/UserRepository.java
+++ store/src/main/java/com/store/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.store.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.store.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	@Query("SELECT u FROM User u WHERE u.username=:username and u.status = 1")
 	User findByUsername(String username);
 }
